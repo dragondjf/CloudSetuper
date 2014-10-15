a38bb6aca371617cebad2cecf855bcde72e9a291
dingjiangfeng <dingjiangfeng@beyondsoft.com>
2014-10-15 18:03:47 +0800

    fix

--- setuper app/installcopy/installcopy.py
+++ setuper app/installcopy/installcopy.py
@@ -5,35 +5,6 @@ import os
 import sys
 import json
 
-a = {
-    "2.png": {
-        "path": "../xl2.png"
-    }, 
-    "3.png": {
-        "path": "../xl3.png"
-    }, 
-    "OutputFolderName": "QFramer", 
-    "1.png": {
-        "path": "../xl1.png"
-    }, 
-    "ExeSource.7z": {
-        "path": "../ExeSource.7z"
-    }, 
-    "4.png": {
-        "path": "../xl4.png"
-    }, 
-    "FinishedOpenFlag": True, 
-    "InstallerUI.exe": {
-        "path": "../InstallerUI.exe"
-    }, 
-    "HomePage": "http://dragondjf.github.io/", 
-    "Icon": {
-        "path": "../setup.ico"
-    }
-}
-
-
-
 def main():
     folderName  = sys.argv[1]
     pacakgejson = os.sep.join([os.getcwd(), folderName, 'package.json'])
