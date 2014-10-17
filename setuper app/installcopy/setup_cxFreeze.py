e5763eac07dc4a5a82c8e206cecd60010dd3ff7a
dingjiangfeng <dingjiangfeng@beyondsoft.com>
2014-10-17 11:00:56 +0800

    make cx_freeze

--- setuper app/installcopy/setup_cxFreeze.py
+++ setuper app/installcopy/setup_cxFreeze.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import shutil
 from cx_Freeze import setup, Executable
 
 sys.argv.append('build')
@@ -32,6 +33,45 @@ def getfiles(path):
             files.append(os.sep.join([dirpath, filename]))
     return files
 
+def copytree(src, dst, symlinks=False):
+    names = os.listdir(src)
+    if not os.path.isdir(dst):
+        os.makedirs(dst)
+        
+    errors = []
+    for name in names:
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks)
+            else:
+                if os.path.isdir(dstname):
+                    os.rmdir(dstname)
+                elif os.path.isfile(dstname):
+                    os.remove(dstname)
+                shutil.copy2(srcname, dstname)
+            # XXX What about devices, sockets etc.?
+        except (IOError, os.error) as why:
+            errors.append((srcname, dstname, str(why)))
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except OSError as err:
+            errors.extend(err.args[0])
+    try:
+        shutil.copystat(src, dst)
+    except WindowsError:
+        # can't copy file access times on Windows
+        pass
+    except OSError as why:
+        errors.extend((src, dst, str(why)))
+    if errors:
+        raise shutil.Error(errors)
+
+
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["os"], "excludes": [""]}
 
@@ -43,8 +83,10 @@ setup(  name = "installcopy",
         version = "1.0",
         description = "Command Line for copy fiel to exe!",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("installcopy.py", base=base, targetDir="distwin")])
+        executables = [Executable("installcopy.py", base=base)])
 
 
+
+copytree(os.sep.join([os.getcwd(), 'build', "exe.win32-2.7"]), os.sep.join([os.getcwd(), "distwin"]))
 for item in ['build']:
     delete_file_folder(item)
